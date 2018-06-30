6014de6d6fb52e847102183a6e4ffddc9a6eefe7
wonyong <zcx5674@gmail.com>
2018-06-30 15:29:20 +0900

    14890 백준 경사로(삼성 기출)

--- src/com/company/cs/baek14890.java
+++ src/com/company/cs/baek14890.java
@@ -11,8 +11,8 @@ public class baek14890 {
 
 	static int[][] map = new int[101][101];
 	static int[][] visited = new int[101][101];
-	static int N, L, result=0; // L : 경사로 길이 
-	static int[] dxArr = {0, 1, 0, -1} , dyArr= {1, 0, -1, 0}; // 동남서북  
+	static int N, L, result = 0; // L : 경사로 길이 
+	static int[] dxArr = {0, 1, 0, -1} , dyArr = {1, 0, -1, 0}; // 동남서북  
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,46 +27,72 @@ public class baek14890 {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		for(int i=1; i<= N; i++)
+		for(int i=1; i<= N; i++) // 행 별로 확인 동쪽 방향  
 		{
-			dfs(i, 0, 0);  // 행별로 확인 동쪽 방향  
+			dfs(i, 1, 0);
+			init();// visited 초기화 작업 
 		}
 		
 		for(int i=1; i<= N; i++)
 		{
-			dfs(0, i, 0);  // 열별로 확인 동쪽 방향  
+			dfs(1, i, 1);  // 열별로 확인 남쪽 방향  
+			init();// visited 초기화 작업
 		}
+		System.out.println(result);
 	}
 	public static void dfs(int dx, int dy, int dir)
 	{
-		if(!(isRange(dx,dy))) return; // 범위 확인  ////////// 
-		int target = map[dx][dy];
-		int rDx = dx +dxArr[dir], rDy = dy + dyArr[dir];
-		int cValue = map[rDx][rDy]; // 비교할 값
+		if(isCheck(dx,dy,dir)) 
+		{
+			result++; return; // 행, 열 별로 확인 후 모든 조건에 만족할 경우 count 
+		}
+		int rDx = dx + dxArr[dir];
+		int rDy = dy + dyArr[dir];
+		int target = map[dx][dy] ,cValue = map[rDx][rDy]; // 다음 경사 확인 
 		
-		if(target == cValue && isRange(rDx,rDy)) dfs(rDx, rDy, dir); // 같은 높이 
-		else if(target > cValue && target - cValue == 1) 
+		if(visited[rDx][rDy] == 0) // 이미 경사로를 놓은 자리인지 확인  
 		{
-			rightBottom(rDx, rDy, dir, 1);
+			if(target == cValue) dfs(rDx,rDy, dir); // flat 한 경우
+			else if(target > cValue && target - cValue == 1) { // 경사로 차이 1, 
+				if(!(isSlope(rDx,rDy,dir, 1 ))) return;
+				
+				for(int i=0; i< L-1; i++) { // 그 다음 값으로바로 이동 
+					rDx += dxArr[dir];
+					rDy += dyArr[dir];
+				}
+				dfs(rDx,rDy,dir);
+			}
+			else if(target < cValue && cValue - target == 1) { // 경사로 차이 1
+				int backDir = direction(dir);
+				if(!(isSlope(dx,dy,backDir, 1))) return;
+				dfs(rDx,rDy,dir);
+			}
+			else return; // 경사로 2 이상 차이  
 		}
-		else if( target < cValue && cValue - target == 1) 
-		{
-			int back = direction(dir); // 해당 방향 반대 방향 확인 
-			leftTop(dx,dy,back, 1);
-		}
+		
 	}
-	public static void rightBottom(int dx, int dy, int dir, int cnt) // 오른쪽 혹은 남쪽 방향 
+	public static boolean isSlope(int dx, int dy, int dir, int cnt) // 경사로가 있을때 경사로 길이만큼 확인 
 	{
-		int rDx = dx +dxArr[dir], rDy = dy + dyArr[dir];
-		if(!isRange(rDx,rDy)) return;
+		if(visited[dx][dy]== 1) return false;
+		visited[dx][dy] = 1;
 		
+		if(cnt == L) return true; // 해당 경사로 길이 만큼  
+		int rDx = dx + dxArr[dir], rDy = dy + dyArr[dir];
+		if(!(isRange(rDx,rDy))) return false; //범위 확인 
+		
+		if(map[dx][dy] == map[rDx][rDy]) return isSlope(rDx,rDy,dir, cnt+1);
+		return false;
+	}
+	public static boolean isCheck(int dx, int dy, int dir) // 행, 열이 전체가 다 연결이 되었는지 확인 
+	{
+		if(dir ==0)
+		{
+			if(dy ==N) return true; // 경사로를 지나서 끝까지 문제 없이 통과 한 경우  
+		}
 		else {
-			if( map[dx][dy] == map[rDx][rDy] ) rightBottom(dx,dy,dir,cnt+1);
+			if(dx ==N) return true;
 		}
-	}
-	public static void leftTop(int dx, int dy, int dir, int cnt) //왼쪽 혹은 북쪽 방향  
-	{
-		int rDx = dx +dxArr[dir], rDy = dy + dyArr[dir];
+		return false;
 	}
 	public static int direction(int dir) // 반대 방향 지정 
 	{
@@ -79,6 +105,12 @@ public class baek14890 {
 		case 3:result=1;break;
 		}
 		return result;
+	}
+	
+	public static void init() {
+		for(int i=1; i<= N; i++)
+			for(int j=1; j<= N; j++)
+				visited[i][j] = 0;
 	}
 	public static boolean isRange(int dx, int dy)
 	{
