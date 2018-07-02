51f26dde2ce5de0efa99031cee56947dc6c3a58f
wonyong <zcx5674@gmail.com>
2018-07-02 20:55:08 +0900

    15684 백준 사다리조작(삼성기출)

--- src/com/company/cs/baek15684.java
+++ src/com/company/cs/baek15684.java
@@ -12,9 +12,7 @@ public class baek15684 {
 
 	static int N, M, H, result = 0; // (H, N) 좌표
 	static boolean flag = false;
-	static int[][] map = new int[51][51]; // 진행 방향을 표시 
-	static int[][] visited = new int[51][51]; // 방문 표시  
-	static int[] dxArr= {1, 0, 0}, dyArr= {0, -1, 1}; // 남 서 동  
+	static int[][] map = new int[32][11]; // 진행 방향을 표시 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -33,23 +31,32 @@ public class baek15684 {
 		}
 		for(int i=0; i<=3; i++)
 		{
-			dfs(0 , i, 1 ,1);
+			dfs(0 , i, 1);
 			if(flag) break;
 		}
 		if(!flag) System.out.println(-1);
 	}
-	public static void dfs(int curCnt ,int lineCnt, int dx, int dy) // 0 ~ 3 개 까지 둘수 있다. 
+	public static void dfs(int curCnt ,int lineCnt, int row) // 0 ~ 3 개 까지 둘수 있다. 
 	{
 		if(flag) return;
 		if(curCnt == lineCnt)
 		{
 			result =0;
-			
-			for(int i=1; i<=N; i++) // 각 열 별로 사다리 검사 시작  
+			int target =0, rDx =0, rDy =0;
+			for(int i=1; i<= N; i++) // 각 열 별로 사다리 검사 시작  
 			{
-				visited[1][i] = 1;
-				move(1, i, i);
-				visited[1][i] = 0;
+				target = i; // target == i 같아야 겠지 
+				for(int j=1; j<= H; j++) // 각 행별로 검사 
+				{
+					if(map[j][target] == 0) continue;
+					else // 방향 전환이 있을때 
+					{
+						if(map[j][target] == 1) target -= 1;
+						else if(map[j][target] == 2) target += 1;
+					}
+				}
+				if(target == i) result++;
+				else break;
 			}
 			if(result == N) {
 				System.out.println(lineCnt); 
@@ -59,36 +66,21 @@ public class baek15684 {
 		}
 		else 
 		{
-			for(int i= dx; i<= H; i++)
+			for(int i= row; i<= H; i++)
 			{
-				for(int j= dy; j<= N-1 ; j++)
+				for(int j= 1; j<= N-1 ; j++)
 				{
 					if(map[i][j] == 0 && map[i][j+1] == 0) 
 					{
 						map[i][j] =2; // 동쪽
 						map[i][j+1] =1; // 서쪽 
-						dfs(curCnt+1, lineCnt, i , j);
+						dfs(curCnt+1, lineCnt, row);
 						map[i][j] =0; // 동쪽
 						map[i][j+1] =0; // 서쪽 
 					}
 				}
 			}
 		}
-	}
-	public static void move(int dx, int dy, int target) {
-		if(isCheck(dx,dy)) {
-			if(target == dy) result++;
-			return;
-		}
-		int dir = map[dx][dy];
-		int rDx = dx + dxArr[dir] , rDy = dy + dyArr[dir];
-		if(visited[rDx][rDy] == 1) {
-			rDx = dx + dxArr[0]; rDy = dy + dyArr[0]; // 아래
-		}
-		visited[rDx][rDy] = 1;
-		move(rDx,rDy, target);
-		visited[rDx][rDy] = 0;
-		
 	}
 	public static boolean isCheck(int dx, int dy) {
 		return dx > 0 && dy > 0 && dx > H & dy <= N;
