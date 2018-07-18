5dd28ab2abb8f8cf64bd805ad998eff6c67bf315
wonyong <zcx5674@gmail.com>
2018-07-18 17:22:40 +0900

    14891 백준 톱니바퀴(삼성 기출)

--- src/com/company/cs/baek14891.java
+++ src/com/company/cs/baek14891.java
@@ -3,72 +3,111 @@ package com.company.cs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
-
-import com.company.cs.baek12100.Point;
 
 /**
  * 톱니바퀴 
  */
 public class baek14891 {
 
-	static LinkedList<Character> arr1 = new LinkedList<>(); // 12시 방향 부터 시계방향 순으로 
-	static LinkedList<Character> arr2 = new LinkedList<>();
-	static LinkedList<Character> arr3 = new LinkedList<>();
-	static LinkedList<Character> arr4 = new LinkedList<>();
-	static LinkedList<LinkedList<Character>> list = new LinkedList<>();
-	static Character[][] map = new Character[4][8];
-	static int[] visited = new int[4];
+	static int[][] map = new int[5][8];
+	static int[] visited;// 4개 톱니 바퀴 
 	static int N;
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		int num =0, dir =0;
-		for(int i=0; i<4; i++) // 톱니 바퀴 셋팅 
+		for(int i=1; i<=4; i++) // 톱니 바퀴 셋팅 
 		{
 			String str = br.readLine();
 			for(int j=0; j< str.length(); j++)
 			{
-				map[i][j] = str.charAt(j);
-				if(i==0) arr1.add(str.charAt(j));
-				else if(i==1) arr2.add(str.charAt(j));
-				else if(i==2) arr3.add(str.charAt(j));
-				else if(i==3) arr4.add(str.charAt(j));
+				map[i][j] = str.charAt(j) - '0'; // 숫자로 저장 
 			}
 		}
 		N = Integer.parseInt(br.readLine());
 		for(int i=0; i< N; i++)
 		{
 			st = new StringTokenizer(br.readLine());
+			visited = new int[5]; // 사용 배열 1 ~ 4 톱니바퀴 갯수 만큼 
 			num = Integer.parseInt(st.nextToken());
 			dir = Integer.parseInt(st.nextToken());
-			visited[num]= num;
-			solve(num,dir);
-			visited[num]= 0;
+			visited[num] = 1;
+			solve(num, dir);
+			visited[num] = 0;
+			
+			turn(num, dir);
 		}
-	}
-	public static void solve(int num, int dir)
-	{
+		printFun(); // 계산 후 출력 
 		
-		if(num+1 <= 4) { // 해당 인덱스에서 오른쪽 톱니 바퀴 //  
-			if(map[num][2] != map[num+1][5]) { // 같지 않으면 반시계 방향 회전 
+	}
+	// 2번 인덱스와 5번 인덱스 비교
+	public static void solve(int num, int dir) // dir   1 시계 방향 /  -1 반시계 방향  
+	{
+		if( num > 4 || num < 1 ) return;
+		
+		if(num+1 <= 4 && visited[num+1] ==0 ) { // 해당 인덱스에서 오른쪽 톱니 바퀴 확인 //  
+			if(map[num][2] != map[num+1][6]) // 같지 않으면 해당 방향의 반대 방향으로 회전 
+			{   
+				 // 현재 톱니 바퀴가 시계방향일때 다음 확인할 톱니바퀴 반시계방향
+				visited[num+1] = 1;
+				solve(num+1, reverse(dir) ); // 반대방향 넣어 주기 
+				visited[num+1] = 0;
 				
+				turn(num+1, reverse(dir));
 			}
 		}
-		// 2번 인덱스와 5번 인덱스 비교 
+		if(num-1 > 0 && visited[num-1] ==0 ) { // 해당 인덱스에서 왼쪽 톱니 바퀴 확인 
+			if(map[num][6] != map[num-1][2]) // 같지 않으면 해당 방향의 반대 방향으로 회전 
+			{
+				visited[num-1] = 1;
+				solve(num-1, reverse(dir) );
+				visited[num-1] = 0;
+				
+				turn(num-1, reverse(dir));
+			}
+		}
 		
 	}
-	public static void leftTurn(int num) // 왼쪽 회전 
+	public static void turn(int num, int dir) // 반시계 방향 회전  
 	{
 		int[] temp = new int[8];
 		for(int i=0; i< 8; i++)
-		{
 			temp[i] = map[num][i];
+		if(dir == -1) // 반시계 방향 
+		{   
+			for(int i=0; i<8; i++)
+			{
+				map[num][i] = temp[(i+1) %8 ]; // 왼쪽으로 shift
+			}
 		}
+		else { // 시계 방향 
+			for(int i=0; i<8; i++)
+			{
+				map[num][ (i+1) % 8 ] = temp[i]; // 으론쪽 으로 shift
+			}
+		}
+	}
+	public static int reverse(int dir) // 반대 방향 
+	{
+		int result =0;
+		switch(dir)
+		{
+		case -1:result=1;break;
+		case 1:result=-1;break;
+		}
+		return result;
+	}
+	public static void printFun()
+	{
+		int result =0;
+		if(map[1][0] == 1) result +=1;
+		if(map[2][0] ==1) result +=2;
+		if(map[3][0] == 1) result+=4;
+		if(map[4][0] ==1) result +=8;
+		System.out.println(result);
 	}
 }
 
