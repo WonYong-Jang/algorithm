25fa2bedef6d7e202ba4ee2069b2012ce3aef6eb
wonyong <zcx5674@gmail.com>
2018-08-05 21:34:10 +0900

    1991 백준 트리순회

--- src/com/company/sds/baek1991.java
+++ src/com/company/sds/baek1991.java
@@ -6,83 +6,74 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 /**
- * 트리 순회 ( preorder, inorder, postorder ) 수정 할 것 
+ * 트리 순회 ( preorder, inorder, postorder ) 
  */
 public class baek1991 {
-
-	static Character[] tree = new Character[27]; // 1 ~ 26
-	
-	static int[] visited = new int[27]; // 전부 
+ 
 	static int N;
+	static Node[] node = new Node[27]; // 1~ 26
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		
-		for(int i=1; i<= 26; i++) // init
+		for(int i=0; i< N; i++)
 		{
-			tree[i] = '.';
+			node[i+1] = new Node((char) ('A'+ i));
 		}
-		
-		int index =1;
-		char root, left, right;
-		tree[1] = 'A'; // 루트 노드 설정 
+		char p, l, r;
+		int index;
 		for(int i=1; i<= N; i++)
 		{
 			st = new StringTokenizer(br.readLine());
-			root = st.nextToken().charAt(0);
-			left = st.nextToken().charAt(0);
-			right = st.nextToken().charAt(0);
-			for(int j=1; j<= 26; j++)
+			p = st.nextToken().charAt(0);
+			l = st.nextToken().charAt(0);
+			r = st.nextToken().charAt(0);
+			
+			index = p-'A'+1;
+			if(l != '.')
 			{
-				if(tree[j] == root) 
-				{
-					if(j*2 <= 26) tree[j*2] = left;
-					if(j*2+1 <= 26) tree[j*2+1] = right;
-					break;
-				}
+				node[index].parent = p-'A'+1;
+				node[index].left = l-'A'+1;
+			}
+			if(r != '.')
+			{
+				node[index].parent = p-'A'+1;
+				node[index].right = r-'A'+1;
 			}
 		}
-		for(int i=1; i<= 26; i++)
-		{
-			System.out.print(tree[i]+" ");
-		}
-		System.out.println();
+		
 		preorder(1);
 		System.out.println();
 		inorder(1);
 		System.out.println();
 		postorder(1);
 	}
-	public static void preorder(int index) // 전위 순회 
+	public static void preorder(int idx)
 	{
-		if(tree[index].equals('.')) return;
-		else 
-		{
-			System.out.print(tree[index]);
-			if(index*2 <= 26) preorder(index*2);
-			if(index*2+1 <= 26) preorder(index*2+1);
-		}
+		System.out.print(node[idx].ch);
+		if( node[idx].left != -1) preorder(node[idx].left);
+		if( node[idx].right != -1) preorder(node[idx].right);
 	}
-	public static void inorder(int index) // 중위 순회  
+	public static void inorder(int idx)
 	{
-		if(tree[index].equals('.')) return;
-		else 
-		{
-			if(index*2 <= 26) inorder(index*2);
-			System.out.print(tree[index]);
-			if(index*2+1 <= 26) inorder(index*2+1);
-		}
+		if( node[idx].left != -1) inorder(node[idx].left);
+		System.out.print(node[idx].ch);
+		if( node[idx].right != -1) inorder(node[idx].right);
 	}
-	public static void postorder(int index) // 후위 순회  
+	public static void postorder(int idx)
 	{
-		if(tree[index].equals('.')) return;
-		else 
-		{
-			if(index*2 <= 26) postorder(index*2);
-			if(index*2+1 <= 26) postorder(index*2+1);
-			System.out.print(tree[index]);
+		if( node[idx].left != -1) postorder(node[idx].left);
+		if( node[idx].right != -1) postorder(node[idx].right);
+		System.out.print(node[idx].ch);
+	}
+	static class Node {
+		char ch;
+		int parent, left, right;
+		Node(char ch) {
+			this.ch = ch; 
+			parent = left = right = -1;
 		}
 	}
 }
