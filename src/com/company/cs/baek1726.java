102b28d44df524d5554ec38896cf8b53057cd7e2
wonyong <zcx5674@gmail.com>
2018-10-12 21:44:31 +0900

    1726 백준 로봇

--- src/com/company/cs/baek1726.java
+++ src/com/company/cs/baek1726.java
@@ -15,7 +15,7 @@ public class baek1726 {
 	static int sdx, sdy, sdir, edx, edy, edir;
 	static int left, right; // 현재 방향 기준 
 	static int[][] map = new int[105][105];
-	static int[][] visit = new int[105][105];
+	static int[][][] visit = new int[4][105][105];
 	static Queue<Node> que = new LinkedList<>();
 	static int[] dxArr = {0, 0, 1, -1}, dyArr = {1, -1, 0, 0};
 	public static void main(String[] args) throws IOException {
@@ -24,14 +24,13 @@ public class baek1726 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		
+		init(); // 초기화 
 		for(int i=1; i<= N; i++)
 		{
 			st = new StringTokenizer(br.readLine());
 			for(int j=1; j<= M; j++)
 			{
 				map[i][j] = Integer.parseInt(st.nextToken());
-				visit[i][j] = 0;
 			}
 		}
 		
@@ -48,11 +47,8 @@ public class baek1726 {
 	}
 	public static void solve()
 	{
-		que.add(new Node(sdx,sdy, sdir, 0));
-		direction(sdir);
-		que.add(new Node(sdx,sdy, left, 1));
-		que.add(new Node(sdx,sdy, right, 1));
-		visit[sdx][sdy] = 1;
+		que.add(new Node(sdx, sdy, sdir));
+		visit[sdir][sdx][sdy] = 0;
 		
 		while(!que.isEmpty())
 		{
@@ -60,28 +56,39 @@ public class baek1726 {
 			
 			if(n.dx == edx && n.dy == edy && n.dir == edir)
 			{
-				System.out.println(n.cmd);
+				System.out.println(visit[n.dir][n.dx][n.dy]);
+				break;
 			}
 			
-			int nx =n.dx, ny =n.dy;
-			for(int i=0; i<3; i++)
+			int nx = n.dx, ny = n.dy;
+			for(int i=0; i< 3; i++)
 			{
 				nx += dxArr[n.dir];
 				ny += dyArr[n.dir];
 				if(!isRange(nx, ny) || map[nx][ny] == 1) break;
-				if(visit[nx][ny] == 1) continue;
 				
-				if(visit[nx][ny] == 0)
+				if(visit[n.dir][nx][ny] == -1) // 앞으로 전진 확인 / 방문한적이 없다면 
 				{
-					que.add(new Node(nx,ny, n.dir, n.cmd+1));
-					direction(n.dir);
-					que.add(new Node(nx,ny, left, n.cmd+2));
-					que.add(new Node(nx,ny, right, n.cmd+2));
+					visit[n.dir][nx][ny] = visit[n.dir][n.dx][n.dy] + 1;
+					que.add(new Node(nx, ny, n.dir));
 				}
+			}
+			direction(n.dir); 
+			
+			if(visit[left][n.dx][n.dy] == -1)
+			{
+				visit[left][n.dx][n.dy] = visit[n.dir][n.dx][n.dy] + 1; // 해당 방향에서 회전 
+				que.add(new Node(n.dx, n.dy, left));
+			}
+			
+			if(visit[right][n.dx][n.dy] == -1)
+			{
+				visit[right][n.dx][n.dy] = visit[n.dir][n.dx][n.dy] + 1; // 해당 방향에서 회전 
+				que.add(new Node(n.dx, n.dy, right));
 			}
 		}
 	}
-	public static void direction(int dir)
+	public static void direction(int dir) // 해당 방향에서 왼쪽 오른쪽 정해줌 
 	{
 		switch(dir)
 		{
@@ -95,22 +102,17 @@ public class baek1726 {
 	{
 		return dx>=1 && dy>=1 && dx<= N && dy <= M;
 	}
-	public static void debug()
+	public static void init()
 	{
-		for(int i=1; i<= N; i++)
-		{
-			for(int j=1; j<= M; j++)
-			{
-				System.out.print(visit[i][j] + " ");
-				
-			}
-			System.out.println();
-		}
+		for(int k=0; k<4; k++)
+			for(int i=1; i<= N; i++)
+				for(int j=1; j<= M; j++)	
+					visit[k][i][j] = -1;
 	}
 	static class Node{
-		int dx, dy, dir, cmd;
-		Node(int a, int b, int c, int d){
-			dx=a; dy=b; dir=c; cmd =d;
+		int dx, dy, dir;
+		Node(int a, int b, int c){
+			dx=a; dy=b; dir=c;
 		}
 	}
 }
