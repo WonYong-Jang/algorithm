493ec9f67f9ac7709bcdb146a8960e38a2835ab6
wonyong <zcx5674@gmail.com>
2019-03-17 21:53:52 +0900

    2188 백준 축사 배정(이분매칭)

---
+++ src/com/company/cs/baek2188.java
@@ -0,0 +1,73 @@
+package com.company.cs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class baek2188 {
+
+	static int N, M;
+	static int[] visit = new int[1005];
+	static int[] match = new int[1005];
+	static ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		for(int i=0; i<= N; i++)
+		{
+			adj.add(new ArrayList<>());
+		}
+		int cnt = 0, num =0;
+		for(int i=1; i<= N; i++)
+		{
+			st = new StringTokenizer(br.readLine());
+			cnt = Integer.parseInt(st.nextToken());
+			for(int j=1; j <= cnt; j++)
+			{
+				num = Integer.parseInt(st.nextToken());
+				adj.get(i).add(num);
+			}
+		}
+		System.out.println(bmatch());
+	}
+	public static int bmatch()
+	{
+		int ret = 0;
+		
+		for(int i=1; i<= N; i++)
+		{
+			for(int j=1; j<= N; j++) visit[j] = 0;
+			if(dfs(i) == 1) ret++;
+		}
+		
+		return ret;
+	}
+	public static int dfs(int cur)
+	{
+		if(visit[cur] == 1) return 0;
+		visit[cur] = 1;
+		
+		for(int next : adj.get(cur))
+		{
+			if(match[next] == 0 || dfs(match[next]) == 1)
+			{
+				match[next] = cur;
+				return 1;
+			}
+		}
+		
+		return 0;
+	}
+}
+
+
+
+
+
+
+
