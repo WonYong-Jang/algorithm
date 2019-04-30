2c6695cdf1325615cab4e52b6deb90c1f06e56ee
wonyong <zcx5674@gmail.com>
2019-04-30 22:19:59 +0900

    12849 백준 본대산책

--- src/com/company/cs/baek12849.java
+++ src/com/company/cs/baek12849.java
@@ -28,35 +28,38 @@ public class baek12849 {
 				dp[i][j] = -1;
 			}
 		}
-		adj.get(0).add(1);
-		adj.get(0).add(2);
-		adj.get(1).add(2);
-		adj.get(1).add(3);
-		adj.get(2).add(3);
-		adj.get(2).add(5);
-		adj.get(3).add(4);
-		adj.get(3).add(5);
-		adj.get(4).add(5);
-		adj.get(4).add(6);
-		adj.get(6).add(7);
-		adj.get(5).add(7);
+		adj.get(0).add(1); adj.get(1).add(0);
+		adj.get(0).add(2); adj.get(2).add(0);
+		adj.get(1).add(2); adj.get(2).add(1);
+		adj.get(1).add(3); adj.get(3).add(1);
+		adj.get(2).add(3); adj.get(3).add(2);
+		adj.get(2).add(5); adj.get(5).add(2);
+		adj.get(3).add(4); adj.get(4).add(3);
+		adj.get(3).add(5); adj.get(5).add(3);
+		adj.get(4).add(5); adj.get(5).add(4);
+		adj.get(4).add(6); adj.get(6).add(4);
+		adj.get(6).add(7); adj.get(7).add(6);
+		adj.get(5).add(7); adj.get(7).add(5);
 		
-		long result = solve(-1, 0, 0);
+		long result = solve(0, 0);
 		System.out.println(result);
 	}
-	public static long solve(int p, int cur, int time)
+	public static long solve(int cur, int time)
 	{
 		long sum = 0;
-		if(dp[cur][time] != -1) return dp[cur][time];
-		else if(time == N) return 1;
+		if(time == N)
+		{
+			if(cur == 0) return 1;
+			else return 0;
+		}
+		else if(dp[cur][time] != -1) return dp[cur][time];
 		else
 		{
+			//System.out.println(cur + " " + time);
 			for(int next : adj.get(cur))
 			{
-				if(next != p)
-				{
-					sum += solve(cur, next, time+1);
-				}
+				sum += ( solve(next, time+1) % mod );
+				sum %= mod;
 			}
 			return dp[cur][time] = sum;
 		}
