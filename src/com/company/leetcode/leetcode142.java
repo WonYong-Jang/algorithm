4693546a38eaf9e6da0861c1843db432b38ea83d
wonyong <zcx5674@gmail.com>
2019-10-31 22:49:14 +0900

    142 leetcode

--- src/com/company/leetcode/leetcode142.java
+++ src/com/company/leetcode/leetcode142.java
@@ -1,13 +1,43 @@
 package com.company.leetcode;
 
-import java.util.HashSet;
+/**
+ * linked list cycle 2
+ *
+ */
 
 public class leetcode142 {
     
-    public static HashSet<Integer> set = new HashSet<>();
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         
     }
     
 }
+
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ 
+public class Solution142 {
+    public HashSet<ListNode> set = new HashSet<>();
+    public int num = 0;
+    public ListNode detectCycle(ListNode head) {
+        
+        if(head == null) return null;
+        if(set.contains(head)) return head;
+        set.add(head);
+        num++;
+        return detectCycle(head.next);
+        
+    }
+}
+
+*/
