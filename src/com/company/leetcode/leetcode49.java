4d5684ddc1d9a473b7c0d14fefba2d496d8d6805
wonyong <zcx5674@gmail.com>
2019-12-08 19:07:43 +0900

    49 leetcode Group Anagrams

--- src/com/company/leetcode/leetcode49.java
+++ src/com/company/leetcode/leetcode49.java
@@ -1,10 +1,64 @@
 package com.company.leetcode;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Approach 1: Categorize by Sorted String
+ */
 public class leetcode49 {
     
     public static void main(String[] args) {
         // TODO Auto-generated method stub
+        List<List<String>> list = new ArrayList<>(); 
+        
+        HashMap<String, List<String>> map = new HashMap<>();
+        
+        String[] strs = {"abcde", "abcde"};
+        
+        
+        
+        
         
     }
     
 }
+
+
+
+/**
+class Solution {
+    
+    public HashMap<String, List<String>> map = new HashMap<>();
+    public int[] count = new int[26];
+    public List<List<String>> groupAnagrams(String[] strs) {
+        
+        for(String cur : strs)
+        {
+            char[] ch = cur.toCharArray();
+            
+            Arrays.fill(count, 0);
+            
+            for(char node : ch)
+            {
+                count[node-'a']++;
+            }
+            
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i< 26; i++)
+            {
+                sb.append(count[i]);
+            }
+            
+            if(!map.containsKey(sb.toString())) {   
+                map.put(sb.toString(), new ArrayList<>());
+            }    
+            map.get(sb.toString()).add(cur);
+            
+        }
+        
+        return new ArrayList(map.values());
+    }
+}
+ */
--- src/com/company/leetcode/leetcode863.java
+++ src/com/company/leetcode/leetcode863.java
@@ -10,3 +10,62 @@ public class leetcode863 {
     }
     
 }
+/*
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+
+class Solution {
+    
+    public ArrayList<Integer>[] adj = new ArrayList[501];
+    public List<Integer> answer = new ArrayList<>();
+    public int[] visit = new int[501];
+    public int cntK;
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int K) {
+        
+        for(int i=0; i<= 500; i++)
+        {
+            adj[i] = new ArrayList<>();
+        }
+        cntK = K;
+        dfs(root, root.left);
+        dfs(root, root.right);
+        
+        solve(target.val, 0);
+        
+        
+        return answer;
+    }
+    public void solve(int cur, int cnt)
+    {
+        if(cnt == cntK)
+        {
+            answer.add(cur);
+            return;
+        }
+        
+        visit[cur] = 1;
+        
+        for(int next : adj[cur])
+        {
+            if(visit[next] == 1) continue;
+            solve(next, cnt+1);
+        }
+    }
+    public void dfs(TreeNode cur, TreeNode next)
+    {
+        if(next == null) return;
+        
+        adj[cur.val].add(next.val);
+        adj[next.val].add(cur.val);
+        
+        dfs(next, next.left);
+        dfs(next, next.right);
+    }
+}
+ */
