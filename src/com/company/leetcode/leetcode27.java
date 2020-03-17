06bdc25ef796297adacb1581c2aea6b3664ccb1e
wonyong <zcx5674@gmail.com>
2020-03-17 20:38:25 +0900

    leetcode 27 Remove Element

---
+++ src/com/company/leetcode/leetcode27.java
@@ -0,0 +1,28 @@
+package com.company.leetcode;
+
+public class leetcode27 {
+    
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
+/**
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        
+        int len = nums.length;
+        int index = 0;
+        for(int i=0; i< len; i++) {
+            
+            if(nums[i] != val) {
+                nums[index++] = nums[i];
+            }
+        }
+        
+        
+        return index;
+    }
+} 
+ */
