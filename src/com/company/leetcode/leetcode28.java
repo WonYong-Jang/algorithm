497d2137c54f63b77f24e539b60da54c03badf7f
wonyong <zcx5674@gmail.com>
2020-07-29 23:02:04 +0900

    feat : indexOf

---
+++ src/com/company/leetcode/leetcode28.java
@@ -0,0 +1,21 @@
+package com.company.leetcode;
+
+public class leetcode28 {
+    
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        
+    }   
+}
+
+/**
+What should we return when needle is an empty string? This is a great question to ask during an interview.
+
+For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+ * 
+ class Solution {
+    public int strStr(String haystack, String needle) {       
+        return haystack.indexOf(needle);
+    }
+}
+ */
