173051b30c82b4e9292ae56f9bc2dc4c20fae976
wonyong <zcx5674@gmail.com>
2020-08-13 21:38:49 +0900

    leetcode 231 Power of Two

---
+++ src/com/company/leetcode/leetcode231.java
@@ -0,0 +1,25 @@
+package com.company.leetcode;
+
+public class leetcode231 {
+    
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        
+    }    
+}
+/**
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        
+        if(n <= 0) return false;
+        
+        if( (n & n-1) == 0) return true;
+        else return false;
+        
+    }
+}
+// 1
+// 10, 01
+// 100, 011
+// 1000, 0111
+ */
