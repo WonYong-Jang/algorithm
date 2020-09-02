d7490b335a95330424b169ff2e93d73780b6ea55
wonyong <zcx5674@gmail.com>
2020-09-02 20:56:25 +0900

    leetcode 257 Binary Tree Paths

---
+++ src/com/company/leetcode/leetcode257.java
@@ -0,0 +1,60 @@
+package com.company.leetcode;
+
+public class leetcode257 {
+    
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+    }
+}
+/**
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/**
+class Solution {
+    public final String next = "->";
+    List<String> answer;
+    public List<String> binaryTreePaths(TreeNode root) {
+        answer = new ArrayList<>();
+        
+        StringBuilder sb = new StringBuilder();
+        
+        search(root, sb);
+        
+        return answer;
+    }
+    public void search(TreeNode root, StringBuilder sb) {
+        
+        if(root == null) return;
+        
+        if(sb.length() > 0 ) {
+            sb.append(next);
+            sb.append(root.val);
+        }
+        else sb.append(root.val);
+        
+        if(root.left == null && root.right == null) {
+            answer.add(sb.toString());
+            return;
+        }
+        StringBuilder leftSb = new StringBuilder(sb.toString());
+        StringBuilder rightSb = new StringBuilder(sb.toString());
+        
+        search(root.left, leftSb);
+        search(root.right, rightSb);
+        
+    }
+}
+ */
