84631f5623852c80dbebe906aecf436b42e05fa3
wonyong <zcx5674@gmail.com>
2020-10-08 22:12:29 +0900

    leetcode 287 Find the Duplicate Number

---
+++ src/com/company/leetcode/leetcode287.java
@@ -0,0 +1,35 @@
+package com.company.leetcode;
+
+public class leetcode287 {
+    
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        
+    }    
+}
+
+/**
+class Solution {
+    public int findDuplicate(int[] nums) {
+        
+        int len = nums.length;
+        
+        int s = 1, e = len-1, mid = 0;
+        
+        while(s < e) {
+            
+            mid = s + (e - s) / 2;
+            int count = 0;
+            for(int i=0; i< len; i++) {
+                if(nums[i] <= mid) count++;
+            }
+            
+            if(count > mid) e = mid;
+            else {
+                s = mid + 1;
+            }
+        }
+        return e;
+    }
+}
+ */
